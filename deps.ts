d271451d6d5517e6e19c3d62ff9159e159e2b026
Andrew Morris <voltrevo@gmail.com>
2021-07-28 18:05:08 +1000

    Add OgsApi with method for listing games of a player

--- deps.ts
+++ deps.ts
@@ -1,2 +1,5 @@
-export { createHash } from "https://deno.land/std@0.93.0/hash/mod.ts";
+export { createHash } from "https://deno.land/std@0.103.0/hash/mod.ts";
+export { Status as HttpStatus } from "https://deno.land/std@0.103.0/http/http_status.ts";
+
+export * as tb from "https://raw.githubusercontent.com/voltrevo/monorepo/c1ff75a/projects/typed-bytes/mod.ts";
 export * as sqlite from "./ext/sqlite.ts";
---
+++ src/OgsApi.ts
@@ -0,0 +1,79 @@
+import { HttpStatus, tb } from "../deps.ts";
+import assert from "./helpers/assert.ts";
+
+class OgsApi {
+  origin: string;
+
+  constructor(origin: string) {
+    const originURL = new URL(origin);
+
+    if (
+      originURL.pathname !== "/" ||
+      originURL.hash !== "" ||
+      originURL.search !== ""
+    ) {
+      throw new Error(`Require plain origin url but received ${origin}`);
+    }
+
+    this.origin = originURL.origin;
+  }
+
+  async get<T>(path: string, type: tb.Bicoder<T>): Promise<T> {
+    assert(path[0] === "/");
+
+    const response = await fetch(`${this.origin}${path}`);
+    assert(response.status === HttpStatus.OK, "Expected HTTP OK");
+
+    const contentType = response.headers.get("content-type") ?? "";
+    assert(contentType.startsWith("application/json"));
+
+    const jsonText = await response.text();
+
+    return tb.JSON.parse(type, jsonText);
+  }
+
+  async *getPaginated<T>(path: string, type: tb.Bicoder<T>): AsyncGenerator<T> {
+    let currentPath: string = path;
+
+    while (true) {
+      const response = await this.get(
+        currentPath,
+        OgsApi.PaginationResponse(type),
+      );
+
+      for (const result of response.results) {
+        yield result;
+      }
+
+      if (response.next === null) {
+        break;
+      }
+
+      currentPath = response.next;
+    }
+  }
+
+  async *Games(playerId: number): AsyncGenerator<OgsApi.Game> {
+    yield* this.getPaginated(`/v1/players/${playerId}/games/`, OgsApi.Game);
+  }
+}
+
+// deno-lint-ignore no-namespace
+namespace OgsApi {
+  export const Game = tb.Object({
+    id: tb.number,
+  });
+
+  export type Game = tb.TypeOf<typeof Game>;
+
+  export function PaginationResponse<T>(type: tb.Bicoder<T>) {
+    return tb.Object({
+      count: tb.number,
+      next: tb.Optional(tb.string),
+      previous: tb.Optional(tb.string),
+      results: tb.Array(type),
+    });
+  }
+}
+
+export default OgsApi;

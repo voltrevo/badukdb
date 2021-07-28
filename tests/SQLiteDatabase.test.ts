d0681ef3679ba860de3032bdded8cfd8386479d0
Andrew Morris <voltrevo@gmail.com>
2021-07-28 17:20:49 +1000

    Add insert test

--- tests/SQLiteDatabase.test.ts
+++ tests/SQLiteDatabase.test.ts
@@ -1,6 +1,8 @@
-import { BoardHash } from "../src/entities.ts";
+import { BoardHash, Player, PlayerId } from "../src/entities.ts";
 import Hash from "../src/Hash.ts";
+import { RandomId } from "../src/Id.ts";
 import SQLiteDatabase from "../src/SQLiteDatabase.ts";
+import { assertEquals } from "./deps.ts";
 
 Deno.test("find moves on non-existent board", async () => {
   const db = new SQLiteDatabase();
@@ -9,4 +11,17 @@ Deno.test("find moves on non-existent board", async () => {
   for await (const _move of db.findMoves(board)) {
     throw new Error("Didn't expect to find anything");
   }
+});
+
+Deno.test("insert player and retrieve", async () => {
+  const db = new SQLiteDatabase();
+
+  const player: Player = {
+    id: PlayerId(RandomId()),
+  };
+
+  db.insertPlayer(player);
+  const retrievedPlayer = await db.lookupPlayer(player.id);
+
+  assertEquals(retrievedPlayer, player);
 });

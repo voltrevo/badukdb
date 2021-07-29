f4d3dee2252463ad6822ad127a55383aba3bdff6
Andrew Morris <voltrevo@gmail.com>
2021-07-29 22:02:20 +1000

    Start setting up client-side tb rpc

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,4 +1,15 @@
+import { tb } from "../../deps.ts";
 import { BoundedGoban, preact } from "./deps.ts";
+
+import { apiPort } from "../constants.ts";
+import WebSocketBufferIO from "../api/WebSocketBufferIO.ts";
+import Protocol from "../api/Protocol.ts";
+
+const bufferIO = new WebSocketBufferIO(
+  new WebSocket(`ws://localhost:${apiPort}/`),
+);
+
+const api = tb.Client(bufferIO, Protocol);
 
 const signMap = [
   [0, 0, 1, 0, -1, -1, 1, 0, 0],

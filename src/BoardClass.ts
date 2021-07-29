cdae822728c45aa1141165b78b53989f670a93ee
Andrew Morris <voltrevo@gmail.com>
2021-07-29 10:28:33 +1000

    Fill in remaining BoardClass methods

--- src/BoardClass.ts
+++ src/BoardClass.ts
@@ -2,10 +2,14 @@ import { tb } from "../deps.ts";
 
 import { Board, BoardHash } from "./entities.ts";
 import Hash from "./Hash.ts";
+import assert from "./helpers/assert.ts";
 import assertExists from "./helpers/assertExists.ts";
 
+const Color = tb.Enum("black", "white");
+type Color = tb.TypeOf<typeof Color>;
+
 const HashlessBoard = tb.Object({
-  colorToPlay: tb.Enum("black", "white"),
+  colorToPlay: Color,
   offboardPoints: tb.number,
   width: tb.size,
   height: tb.size,
@@ -44,32 +48,184 @@ export default class BoardClass {
     return { hash, ...this.data };
   }
 
+  IndexAndShift(x: number, y: number) {
+    this.assertBounds(x, y);
+
+    const doubleBitPos = (y - 1) * this.data.width + (x - 1);
+
+    return {
+      index: Math.floor(doubleBitPos / 4),
+      shift: 2 * (doubleBitPos % 4),
+    };
+  }
+
+  checkBounds(x: number, y: number) {
+    return (
+      (1 <= x && x <= this.data.width) &&
+      (1 <= y && y <= this.data.height)
+    );
+  }
+
+  assertBounds(x: number, y: number) {
+    assert(
+      this.checkBounds(x, y),
+      [
+        `(${x}, ${y}) is out of bounds on`,
+        `${this.data.width}x${this.data.height} board`,
+      ].join(" "),
+    );
+  }
+
   read(x: number, y: number): CellState {
-    const doubleBitPos = y * this.data.width + x;
-    const byte = this.data.content[Math.floor(doubleBitPos / 4)];
+    const { index, shift } = this.IndexAndShift(x, y);
+    const byte = this.data.content[index];
     let doubleBit = byte;
-    doubleBit >>= 2 * (doubleBitPos % 4);
+    doubleBit >>= shift;
     doubleBit = doubleBit % 4;
 
     return assertExists(cellStates[doubleBit]);
   }
 
   write(x: number, y: number, state: CellState) {
-    const doubleBitPos = y * this.data.width + x;
+    const { index, shift } = this.IndexAndShift(x, y);
+
     const doubleBit = cellStates.indexOf(state);
 
-    const contentIndex = Math.floor(doubleBitPos / 4);
-    let byte = this.data.content[contentIndex];
-
-    const bitOffset = 2 * (doubleBitPos % 4);
-    const mask = 0b11 << bitOffset;
+    let byte = this.data.content[index];
+    const mask = 0b11 << shift;
 
     // Clear this location
     byte = byte & (~mask);
 
     // Add new state
-    byte = byte | (doubleBit << bitOffset);
+    byte = byte | (doubleBit << shift);
 
-    this.data.content[contentIndex] = byte;
+    this.data.content[index] = byte;
+  }
+
+  pass() {
+    this.data.colorToPlay = this.data.colorToPlay === "black"
+      ? "white"
+      : "black";
+  }
+
+  *IterateAdjacents(x: number, y: number): Generator<{ x: number; y: number }> {
+    const candidates = [
+      { x: x - 1, y },
+      { x: x + 1, y },
+      { x, y: y - 1 },
+      { x, y: y + 1 },
+    ];
+
+    for (const candidate of candidates) {
+      if (this.checkBounds(candidate.x, candidate.y)) {
+        yield candidate;
+      }
+    }
+  }
+
+  *IterateRegion(
+    x: number,
+    y: number,
+  ): Generator<{ x: number; y: number }> {
+    const cellState = this.read(x, y);
+
+    yield { x, y };
+
+    const uncheckedPoints = [{ x, y }];
+    const checkedPoints = new Set<string>();
+
+    while (true) {
+      const point = uncheckedPoints.shift();
+
+      if (point === undefined) {
+        break;
+      }
+
+      checkedPoints.add(`${point.x},${point.y}`);
+
+      for (const adj of this.IterateAdjacents(point.x, point.y)) {
+        if (
+          this.read(adj.x, adj.y) !== cellState ||
+          checkedPoints.has(`${adj.x},${adj.y}`)
+        ) {
+          continue;
+        }
+
+        yield adj;
+        uncheckedPoints.push(adj);
+      }
+    }
+  }
+
+  *IterateLiberties(x: number, y: number): Generator<{ x: number; y: number }> {
+    const cellState = this.read(x, y);
+    assert(cellState === "black" || cellState === "white");
+
+    const reported = new Set<string>();
+
+    for (const point of this.IterateRegion(x, y)) {
+      for (const adj of this.IterateAdjacents(point.x, point.y)) {
+        if (this.read(adj.x, adj.y) !== "empty") {
+          continue;
+        }
+
+        const adjStr = `${adj.x},${adj.y}`;
+
+        if (reported.has(adjStr)) {
+          continue;
+        }
+
+        yield adj;
+        reported.add(adjStr);
+      }
+    }
+  }
+
+  isCaptured(x: number, y: number): boolean {
+    for (const _liberty of this.IterateLiberties(x, y)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  capture(x: number, y: number) {
+    const color = this.read(x, y);
+    assert(color !== "empty");
+
+    const pointDelta = color === "white" ? 1 : -1;
+
+    for (const point of this.IterateRegion(x, y)) {
+      this.write(point.x, point.y, "empty");
+      this.data.offboardPoints += pointDelta;
+    }
+  }
+
+  play(x: number, y: number, color: Color) {
+    this.write(x, y, color);
+    const opponentColor = BoardClass.OpponentColor(color);
+
+    for (const adj of this.IterateAdjacents(x, y)) {
+      if (this.read(adj.x, adj.y) !== opponentColor) {
+        continue;
+      }
+
+      if (!this.isCaptured(adj.x, adj.y)) {
+        continue;
+      }
+
+      this.capture(adj.x, adj.y);
+    }
+
+    if (this.isCaptured(x, y)) {
+      this.capture(x, y);
+    }
+
+    this.data.colorToPlay = opponentColor;
+  }
+
+  static OpponentColor(color: Color): Color {
+    return color === "black" ? "white" : "black";
   }
 }

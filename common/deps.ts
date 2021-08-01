8f2988437d56a54c73d29d4e42715bc7f5dc3c43
Andrew Morris <voltrevo@gmail.com>
2021-08-02 09:16:25 +1000

    Update typed-bytes

--- common/deps.ts
+++ common/deps.ts
@@ -1,4 +1,4 @@
 export { Keccak256 } from "https://deno.land/std@0.103.0/hash/_sha3/keccak.ts";
 export { Status as HttpStatus } from "https://deno.land/std@0.103.0/http/http_status.ts";
 
-export * as tb from "https://raw.githubusercontent.com/voltrevo/monorepo/7e276c9/projects/typed-bytes/mod.ts";
+export * as tb from "https://raw.githubusercontent.com/voltrevo/monorepo/3a6b45e/projects/typed-bytes/mod.ts";

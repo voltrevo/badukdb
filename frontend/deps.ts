e380cd023243d2b25cd374663cfa1425a57f02c2
Andrew Morris <voltrevo@gmail.com>
2021-08-02 09:25:11 +1000

    Fix preact import

--- frontend/deps.ts
+++ frontend/deps.ts
@@ -1,7 +1,7 @@
 export * from "../common/deps.ts";
 
 export { BoundedGoban } from "https://cdn.skypack.dev/@sabaki/shudan@v1.5.4";
-export * as preact from "https://cdn.skypack.dev/-/preact@v10.5.14-cWu6Yly5RdZVWuUJuqUw/dist=es2020,mode=imports/optimized/preact.js";
+export * as preact from "https://cdn.skypack.dev/preact@v10.5.14";
 
 export const bundlerLocation =
   "https://raw.githubusercontent.com/voltrevo/Bundler/bf741fe";

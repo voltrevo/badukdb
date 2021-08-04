8cafcc4570479c2ac3b3149083c5fdcba8ca9728
Andrew Morris <voltrevo@gmail.com>
2021-08-04 14:17:46 +1000

    Fix ./programs/apiServer.ts

--- programs/apiServer.ts
+++ programs/apiServer.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S deno --allow-net --allow-read --allow-write --allow-env
+#!/usr/bin/env -S deno run --allow-net --allow-read --allow-write --allow-env
 
 import serveAPI from "../backend/serveAPI.ts";
 
